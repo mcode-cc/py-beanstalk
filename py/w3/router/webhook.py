8fdb8941f75d30071d4ce5dadbacba5ca42b672f
Andrey Alexandrov <andrey.aleksandrov@gmail.com>
2017-03-23 18:20:43 +0300

    webhook from bitbucket

--- py/w3/router/webhook.py
+++ py/w3/router/webhook.py
@@ -2,12 +2,14 @@
 
 from flask import Flask
 from flask import request
+import git
+
 app = Flask(__name__)
 
 HOST = '127.0.0.1'
 PORT = 5000
 TEXT_HTML = 'Webhook server online! Go to <a href="https://bitbucket.com">Bitbucket</a> to configure your repository webhook for <a href="%s/webhook">%s/webhook</a>'
-
+GIT_PATH = '/var/www/xshl'
 
 def _intro():
     print 'Webhook server online! Go to http://%s:%d' % (HOST, PORT)
@@ -31,6 +33,11 @@ def tracking():
         commit_url = data['push']['changes'][0]['new']['target']['links']['html']['href']
         print 'Webhook received! %s committed %s' % (commit_author, commit_hash)
         print commit_url
+        try:
+            g = git.cmd.Git(GIT_PATH)
+            g.pull()
+        except Exception, e:
+            print str(e)
         return 'OK'
     else:
         return _html(request)
